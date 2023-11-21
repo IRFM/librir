b215355bd24d0a937ad483b5f404891174172440
dubusster <leo@dubus.io>
2023-11-21 15:18:19 +0100

    style: :art: examples format

--- examples/cpp_build_example/test.cpp
+++ examples/cpp_build_example/test.cpp
@@ -1,9 +1,9 @@
 #include "h264.h"
 
-int main(int, char ** const)
+int main(int, char **const)
 {
 	rir::H264_Saver saver;
-	saver.open("test.bin",640,512,512,50);
-	
+	saver.open("test.bin", 640, 512, 512, 50);
+
 	return 0;
 }
