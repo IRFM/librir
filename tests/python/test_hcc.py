6523805227f8d708d6f5f1a4b16e7eef967e6008
DUBUS Léo 243615 <leo.dubus@cea.fr>
2024-03-13 14:19:21 +0100

    test: :white_check_mark: add and pass test

---
+++ tests/python/test_hcc.py
@@ -0,0 +1,31 @@
+import os
+from pathlib import Path
+from typing import Optional
+from librir.video_io import IRMovie
+from librir.video_io.rir_video_io import change_hcc_external_blackbody_temperature
+import pytest
+
+
+@pytest.fixture
+def hcc_filename():
+    p = os.environ.get("HCC_FILE_TEST", None)
+    if p is not None:
+        return Path(p)
+
+
+@pytest.mark.parametrize("temperature", [300])
+def test_change_hcc_external_blackbody_temperature(
+    hcc_filename: Optional[Path], temperature
+):
+    if hcc_filename is None or not hcc_filename.exists():
+        pytest.skip("No HCC test file")
+    with IRMovie.from_filename(hcc_filename) as mov:
+        old_temperature = mov.attributes.get("ExternalBlackBodyTemperature (cC)", 0)
+    old_temperature = float(old_temperature)
+    change_hcc_external_blackbody_temperature(hcc_filename, temperature)
+    with IRMovie.from_filename(hcc_filename) as mov:
+        new_temperature = mov.attributes.get("ExternalBlackBodyTemperature (cC)", 0)
+    assert float(new_temperature) == temperature
+    assert temperature != old_temperature
+
+    change_hcc_external_blackbody_temperature(hcc_filename, old_temperature)
